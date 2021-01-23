0ffbb92ebf22f92d6202046ad887ffa9fb26a7dc
Aaron Harris <aamharris@gmail.com>
2021-01-22 21:32:13 -0600

    update dice containter tests

--- src/dice-container/DiceContainer.test.tsx
+++ src/dice-container/DiceContainer.test.tsx
@@ -6,34 +6,35 @@ import { YahtzeeDice } from '../App';
 describe('DiceContainer', () => {
     describe('Dice', () => {
         const renderDiceContainer = (dice: YahtzeeDice[]) => {
-            render(<DiceContainer dice={dice} 
-                onDiceRolled={jest.fn()} 
+            render(<DiceContainer dice={dice}
+                onDiceRolled={jest.fn()}
                 onDiceClicked={jest.fn()}
                 canRollDice={true} />);
         }
+        
         it('displays all dice values', () => {
             const dice: YahtzeeDice[] = [
-                { id: 1, value: 1, isLocked: false},
-                { id: 2, value: 2, isLocked: false},
+                { id: 1, value: 1, isLocked: false },
+                { id: 2, value: 2, isLocked: false },
             ]
-    
+
             renderDiceContainer(dice);
             expect(screen.getByText("1")).toBeInTheDocument();
             expect(screen.getByText("2")).toBeInTheDocument();
         });
-    
+
         it('displays empty dice value when dice value is undefined', () => {
             const dice: YahtzeeDice[] = [
-                { id: 1, value: undefined, isLocked: false},
+                { id: 1, value: undefined, isLocked: false },
             ]
-    
+
             renderDiceContainer(dice);
             expect(screen.getByText("-")).toBeInTheDocument();
         });
 
         it('highlights locked dice', () => {
             const dice: YahtzeeDice[] = [
-                { id: 1, value: undefined, isLocked: true},
+                { id: 1, value: undefined, isLocked: true },
             ]
 
             renderDiceContainer(dice);
@@ -45,45 +46,44 @@ describe('DiceContainer', () => {
         const onRollClicked = jest.fn();
 
         const createDiceWithValue = (value: number | undefined, isLocked: boolean = false): YahtzeeDice => {
-            return { id: 1, value: value, isLocked: isLocked}; 
+            return { id: 1, value: value, isLocked: isLocked };
         }
 
         const renderDiceContainer = (dice: YahtzeeDice): void => {
-            render(<DiceContainer 
-                dice={[dice]} 
-                onDiceRolled={onRollClicked} 
-                onDiceClicked={() => {}}
+            render(<DiceContainer
+                dice={[dice]}
+                onDiceRolled={onRollClicked}
+                onDiceClicked={() => { }}
                 canRollDice={true}
-                 />);
+            />);
         }
 
         beforeEach(() => {
-            jest.resetAllMocks();            
+            jest.resetAllMocks();
         });
 
         it('rolls a 1 when math.random is 0', async () => {
             let dice = createDiceWithValue(undefined);
             jest.spyOn(global.Math, 'random').mockReturnValue(0.0);
             renderDiceContainer(dice);
-            fireEvent.click(screen.getByRole('button', {name: 'Roll'}));
-            expect(onRollClicked).toBeCalledWith([{...dice, value: 1}])
+            fireEvent.click(screen.getByRole('button', { name: 'Roll' }));
+            expect(onRollClicked).toBeCalledWith([{ ...dice, value: 1 }])
         });
 
         it('rolls a 6 when math.random is 0.9', async () => {
             let dice = createDiceWithValue(undefined);
             jest.spyOn(global.Math, 'random').mockReturnValue(0.9);
             renderDiceContainer(dice);
-            fireEvent.click(screen.getByRole('button', {name: 'Roll'}));
-            expect(onRollClicked).toBeCalledWith([{...dice, value: 6}])
+            fireEvent.click(screen.getByRole('button', { name: 'Roll' }));
+            expect(onRollClicked).toBeCalledWith([{ ...dice, value: 6 }])
         });
 
         it('does not roll dice if dice is locked', () => {
             let dice = createDiceWithValue(2, true);
             jest.spyOn(global.Math, 'random').mockReturnValue(0.0);
             renderDiceContainer(dice);
-            fireEvent.click(screen.getByRole('button', {name: 'Roll'}));
-            expect(onRollClicked).toBeCalledWith([{...dice, value: 2}])
+            fireEvent.click(screen.getByRole('button', { name: 'Roll' }));
+            expect(onRollClicked).toBeCalledWith([{ ...dice, value: 2 }])
         });
     });
-    
 })
