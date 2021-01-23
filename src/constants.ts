bdda34d467d0f1f4065bb0b7df2bec732e84782d
Aaron Harris <aamharris@gmail.com>
2021-01-22 21:27:30 -0600

    add score constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export const FULL_HOUSE_SCORE = 25;
+export const SM_STRAIGHT_SCORE = 30;
+export const LG_STRAIGHT_SCORE = 40;
+export const YAHTZEE_SCORE = 50;
+export const YAHTZEE_BONUS_SCORE = 100;
